b22dc8098dadba9b5501229694e2ad97d63ec3d6
Olisail <olivier.sailly.59@gmail.com>
2024-09-19 11:08:42 +0200

    feature: use Duende Demo instead of GitHub

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig, devices } from '@playwright/test';
 import { defineBddConfig, cucumberReporter } from 'playwright-bdd';
+import { config } from 'dotenv';
+import { existsSync } from 'fs';
+import path from 'path';
 
 const testDir = defineBddConfig({
 	features: 'tests/features/**/*.feature',
 	steps: 'tests/features/**/*.stepdefinitions.ts',
 });
+
+const envPath = path.resolve(__dirname, '.env');
+if(existsSync(envPath)){
+	config({ path: envPath });
+}
 
 export default defineConfig({
 	testDir,
--- tests/features/home/homepage.stepdefinitions.ts
+++ tests/features/home/homepage.stepdefinitions.ts
@@ -1,13 +1,17 @@
 import { expect } from '@playwright/test';
 import { Given, When, Then } from '../../utils/fixtures';
 
-Given('I am on the GitHub home page', async ({ page }) => {
-	await page.goto('https://github.com');
-	await expect(page).toHaveTitle(/GitHub/);
+Given('I am on the Duende Demo home page', async ({ page }) => {
+	await page.goto('https://demo.duendesoftware.com/diagnostics');
+	await expect(page).toHaveTitle(/Duende IdentityServer/);
 });
 
-When('I fill {string} in the Email input field', async ({ page }, name: string) => {
-	await page.locator('#hero_user_email').fill(name);
+When('I fill in the Login input field', async ({ page }) => {
+	await page.locator('#Input_Username').fill(process.env.DUENDE_USERNAME);
+});
+
+When('I fill in the Password input field', async ({ page }) => {
+	await page.locator('#Input_Password').fill(process.env.DUENDE_PASSWORD);
 });
 
 When('I click on the {string} button', async ({ page }, name: string) => {
