6df70d6352c453b1561fb2cf166c2f616807d6e2
Antoine <heinrich.antoine.syn@gmail.com>
2025-06-01 20:10:55 +0200

    feat: remove configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,2 @@
 import { defineConfig, devices } from '@playwright/test';
-import { defineBddConfig, cucumberReporter } from 'playwright-bdd';
-
-const testDir = defineBddConfig({
-	features: 'tests/features/**/*.feature',
-	steps: 'tests/features/**/*.stepdefinitions.ts',
-});
-
-export default defineConfig({
-	testDir,
-	reporter: [cucumberReporter('html', { outputFile: 'cucumber-report/report.html' })],
-	projects: [
-		{
-			name: 'chromium',
-			use: { ...devices['Desktop Chrome'] },
-		},
-	],
-});
+import { defineBddConfig } from 'playwright-bdd';
