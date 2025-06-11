3de4ca5a3781790f635d10bcee669bce8bf75e51
toinouu <toinouu@MacBook-Pro-de-toinouu.local>
2025-06-12 00:04:58 +0200

    feat(Fixture): add auth fixture exemple

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,26 +2,20 @@ import { defineConfig, devices } from '@playwright/test';
 import { defineBddConfig, cucumberReporter } from 'playwright-bdd';
 
 const testDir = defineBddConfig({
-	features: 'tests/features/**/*.feature',
-	steps: 'tests/features/**/*.stepdefinitions.ts',
-	importTestFrom: 'tests/utils/fixtures.ts',
-	disableWarnings: {
-		importTestFrom: true,
-	},
+  features: 'tests/features/**/*.feature',
+  steps: 'tests/features/**/*.stepdefinitions.ts',
+  importTestFrom: 'tests/utils/fixtures.ts',
+  disableWarnings: {
+    importTestFrom: true,
+  },
 });
 
 export default defineConfig({
-	testDir,
-	projects: [
-		{
-			name: 'auth',
-			testMatch: '**/auth.setup.ts',
-			testDir: 'tests/utils',
-		},
-		{
-			name: 'chromium',
-			use: { ...devices['Desktop Chrome'], storageState: 'tests/.auth/user.json' },
-			dependencies: ['auth'],
-		},
-	],
+  testDir,
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
 });
--- tests/utils/fixtures.ts
+++ tests/utils/fixtures.ts
@@ -1,7 +1,26 @@
 import { test as base, createBdd } from 'playwright-bdd';
+import { expect } from 'playwright/test';
 
-type Fixtures = {};
+type Fixtures = {
+  auth: { loginFunction: () => Promise<void> };
+};
 
-export const test = base.extend<Fixtures>({});
+export const test = base.extend<Fixtures>({
+  auth: async ({ page }, use) => {
+    const loginFunction = async () => {
+      await page.goto('https://demo.duendesoftware.com/diagnostics');
+      await expect(page).toHaveTitle(/Duende IdentityServer/);
+
+      await page.locator('#Input_Username').fill('bob');
+      await page.locator('#Input_Password').fill('bob');
+
+      await page.getByRole('button', { name: 'Login' }).click();
+      //retour à la page d'accueil après la connexion
+      await page.goto('https://demo.duendesoftware.com/');
+    };
+
+    await use({ loginFunction });
+  },
+});
 
 export const { Given, When, Then } = createBdd(test);
