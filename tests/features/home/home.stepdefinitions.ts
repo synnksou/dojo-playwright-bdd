2cd7a464afac74823183ef51167d67823cad3a7c
toinouu <toinouu@MacBook-Pro-de-toinouu.local>
2025-06-11 23:06:13 +0200

    fix(BDD): update lib

--- tests/features/home/home.stepdefinitions.ts
+++ tests/features/home/home.stepdefinitions.ts
@@ -4,13 +4,14 @@ import { expect } from 'playwright/test';
 const { Given, When, Then } = createBdd();
 
 Given("Je suis sur la page d'accueil de Duende", async ({ page }) => {
-	await page.goto('https://demo.duendesoftware.com/');
+  await page.goto('https://demo.duendesoftware.com/');
 });
 
 When('La page est complètement chargée', async ({ page }) => {
-	await page.waitForLoadState('domcontentloaded');
+  await page.waitForLoadState('domcontentloaded');
+  console.log('Page chargée');
 });
 
 Then('Je devrais voir le titre {string}', async ({ page }, expectedText: string) => {
-	await expect(page.getByRole('link', { name: expectedText })).toBeVisible();
+  await expect(page.getByRole('link', { name: expectedText })).toBeVisible();
 });
