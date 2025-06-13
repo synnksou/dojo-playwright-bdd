a928202c81d786422ec17fc2ca375daa4317fc8e
toinouu <toinouu@MacBook-Pro-de-toinouu.local>
2025-06-13 10:35:24 +0200

    feat(PW): change expect

--- tests/features/profil/profile.stepdefinitions.ts
+++ tests/features/profil/profile.stepdefinitions.ts
@@ -1,17 +1,18 @@
+import { expect } from 'playwright/test';
 import { Given, When, Then } from '@utils/fixtures';
 
 Given('Je suis authentifié sur le Duende', async ({ page }) => {
-	await page.goto('https://demo.duendesoftware.com');
+  await page.goto('https://demo.duendesoftware.com');
 });
 
 When('Je navigue vers le profil', async ({ page }) => {
-	await page.getByRole('link', { name: 'Go ' }).nth(1).click();
+  await page.getByRole('link', { name: 'Go ' }).nth(1).click();
 });
 
 Then('Je devrais voir les cookies', async ({ page }) => {
-	await page.getByRole('heading', { name: 'Properties' }).click();
+  await expect(page.getByRole('heading', { name: 'Properties' })).toBeVisible();
 });
 
 Then('Je devrais voir les droits', async ({ page }) => {
-	await page.getByRole('heading', { name: 'Claims' }).click();
+  await expect(page.getByRole('heading', { name: 'Claims' })).toBeVisible();
 });
